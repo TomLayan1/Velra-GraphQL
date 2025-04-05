e4575964023ecc6338b25783369c0fa4971dac34
TomLayan1 <afolayantomisin1919@gmail.com>
2025-04-05 20:56:55 +0100

    Added a demo products data in the schema file

--- schema/schema.ts
+++ schema/schema.ts
@@ -1,12 +1,64 @@
-import { buildSchema } from 'graphql';
-import { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLFloat, GraphQLInt, GraphQLID } from 'graphql';
+import { GraphQLSchema } from 'graphql';
+import { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLFloat, GraphQLInt } from 'graphql';
+
+
+let products = [
+  {
+    id: "1",
+    name: "Stackable Chair",
+    image: "http://localhost:4000/images/products/product1.jpg",
+    price: 87.00,
+    category: { id: "1", name: "Chair" },
+    details: ""
+  },
+  {
+    id: "2",
+    name: "Lamp Tool",
+    image: "http://localhost:4000/images/products/product2.jpg",
+    price: 35.00,
+    category: { id: "1", name: "Lamp" },
+    details: "Soft leather armchair"
+  },
+  {
+    id: "3",
+    name: "Dining Chair",
+    image: "http://localhost:4000/images/products/product3.jpg",
+    price: 55.00,
+    category: { id: "1", name: "Chair" },
+    details: "Soft leather armchair"
+  },
+  {
+    id: "4",
+    name: "Hand Base Lamp",
+    image: "http://localhost:4000/images/products/product4.jpg",
+    price: 35.00,
+    category: { id: "1", name: "Lamp" },
+    details: "Soft leather armchair"
+  },
+  {
+    id: "5",
+    name: "Stylish Chair",
+    image: "http://localhost:4000/images/products/product5.jpg",
+    price: 45.00,
+    category: { id: "1", name: "Chair" },
+    details: "Soft leather armchair"
+  },
+  {
+    id: "6",
+    name: "Vintage Chair",
+    image: "http://localhost:4000/images/products/product6.jpg",
+    price: 65.00,
+    category: { id: "1", name: "Furniture" },
+    details: "Soft leather armchair"
+  },
+];
 
 
 // Define the Category type
 const CategoryType  = new GraphQLObjectType({
   name: 'Categories',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     name: { type: GraphQLString }
   }),
 });
@@ -15,7 +67,7 @@ const CategoryType  = new GraphQLObjectType({
 const ProductsType = new GraphQLObjectType({
   name: 'Products',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     name: { type: GraphQLString },
     image: { type: GraphQLString },
     price: { type: GraphQLFloat },
@@ -24,6 +76,7 @@ const ProductsType = new GraphQLObjectType({
   }),
 });
 
+// Define the CartItem type
 const CartItemType = new GraphQLObjectType({
   name: 'CartItems',
   fields: () => ({
@@ -32,10 +85,11 @@ const CartItemType = new GraphQLObjectType({
   }),
 });
 
+
 const CartType = new GraphQLObjectType({
   name: 'Cart',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     items: { type: new GraphQLList(CartItemType) },
     total: { type: GraphQLFloat }
   }),
@@ -44,6 +98,17 @@ const CartType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-
+    products: {
+      type: ProductsType,
+      args: { id: { type: GraphQLString }},
+      resolve(parent, args) {
+        // Function to get data from db
+      }
+    }
   }
+})
+
+// Export RootQuery
+export default new GraphQLSchema({
+  query: RootQuery
 })
--- server/app.ts
+++ server/app.ts
@@ -1,29 +1,19 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
+import schema from '../schema/schema'
+import path from 'path';
 
-// Define a basic GraphQL schema
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// Define a resolver for the schema
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
 
 // Initialize express app
 const app = express();
 
+// Serve static images from the "images" folder
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 // Express-graph module allows express understand graphql
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true, // enables the GraphiQL UI at /graphql
+  schema,
+  graphiql: true,
 }))
 
 // Tell the app to listen to a port e.g port 4000 and a callback function that will be executed when the app starts listening to port 4000
